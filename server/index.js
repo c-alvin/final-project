78678fa94d3bd58133a0e78043d93f7ef82fed89
Alvin Cho <alvinxcho2@gmail.com>
2022-09-13 04:56:02 +0000

    got my request to the api to work

--- server/index.js
+++ server/index.js
@@ -2,13 +2,26 @@ require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
+const fetch = require('node-fetch');
 
 const app = express();
 
 app.use(staticMiddleware);
 
-app.get('/api/hello', (req, res) => {
-  res.json({ hello: 'world' });
+app.get('/api/search', (req, res) => {
+  // console.log(req.query.term);
+  const search = req.query.term;
+  fetch('https://api.igdb.com/v4/games', {
+    method: 'POST',
+    headers: {
+      'Client-ID': process.env.CLIENT_ID,
+      Authorization: 'Bearer wb9acv3ulq4tsa3zc27zkli61edii0'
+    },
+    body: `fields cover.*, name, platforms.*; search: "${search}";`
+  })
+    .then(res => res.json())
+    .then(data => res.status(201).json(data))
+    .catch(err => console.error(err));
 });
 
 app.use(errorMiddleware);
