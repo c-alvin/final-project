4ff96a078893da5a18a362ca81cea0fdbd6a353b
Alvin Cho <alvinxcho2@gmail.com>
2022-09-19 19:53:18 +0000

    Complted issue-4-user-can-rate-a-game

--- client/components/star-rating.jsx
+++ client/components/star-rating.jsx
@@ -6,7 +6,7 @@ export default class StarRating extends React.Component {
       new Array(5).fill().map((star, index) => {
         index += 1;
         return (
-          <i key={index} data-index={index} onClick={this.props.onClick} className={index <= this.props.rating ? `fa-solid fa-star gold ${this.props.starSize}` : `fa-regular fa-star ${this.props.starSize} test-white`}></i>
+          <i key={index} data-index={index} onClick={this.props.onClick} className={index <= this.props.rating ? `fa-solid fa-star gold ${this.props.starSize}` : `fa-regular fa-star ${this.props.starSize} star-border-white`}></i>
         );
       }));
   }
